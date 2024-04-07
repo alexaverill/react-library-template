fb9cc80314e325d6cfb2ae6c949ba54d05bcbe90
alexaverill <alex.averill2013@gmail.com>
2024-04-07 13:04:32 -0500

    Adding in a CRA config loader

--- dist/ConfigLoader/ConfigLoader.js
+++ dist/ConfigLoader/ConfigLoader.js
@@ -1,23 +1,32 @@
-var l = { VITE_LOG_LEVEL: "DEBUG", BASE_URL: "/", MODE: "production", DEV: !1, PROD: !0, SSR: !1 };
+var t = { VITE_LOG_LEVEL: "DEBUG", BASE_URL: "/", MODE: "production", DEV: !1, PROD: !0, SSR: !1 };
 function n() {
-  return [...a()];
+  let l = a(), e = i();
+  return [...l, ...e];
 }
-function i() {
-  let t = n();
-  for (let e of t)
+function u() {
+  let l = n();
+  for (let e of l)
     if (console.log(e), e.LOG_LEVEL)
       return e.LOG_LEVEL;
   return null;
 }
 function a() {
-  let t = [];
-  for (let e in l) {
-    let r = l[e];
-    e.includes("VITE_") && (e = e.replace("VITE_", "")), t.push({ [e]: r });
+  let l = [];
+  for (let e in t) {
+    let r = t[e];
+    e.includes("VITE_") && (e = e.replace("VITE_", "")), l.push({ [e]: r });
   }
-  return t;
+  return l;
+}
+function i() {
+  let l = [];
+  for (let e in process.env) {
+    let r = process.env[e];
+    e.includes("REACT_") && (e = e.replace("REACT_", "")), l.push({ [e]: r });
+  }
+  return l;
 }
 export {
-  i as GetDebugLevel,
+  u as GetDebugLevel,
   n as LoadConfig
 };
